2b46bf0a04cc3690988e42dbbd33e759955e3576
piotr-ost <piotr.ostrowski@aol.com>
2021-09-15 13:42:16 +0200

    update setup script

--- setup.sh
+++ setup.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-git clone https://github.com/piotrostr/helping-the-parents && 
-    cd helping-the-parents && 
-    git clone https://github.com/swook/EVE &&
-    git clone https://github.com/patrikhuber/eos && 
-    sudo apt-get update && 
-    sudo apt-get install git-lfs &&
+{
+    git clone https://github.com/patrikhuber/eos &&
+    apt-get update && 
+    apt-get install git-lfs &&
+    apt-get install cmake &&
     git-lfs pull && 
     pip install -r requirements.txt 
+
+} || {
+
+    git clone https://github.com/patrikhuber/eos &&
+    sudo apt-get update && 
+    sudo apt-get install git-lfs &&
+    sudo apt-get install cmake &&
+    git-lfs pull && 
+    pip install -r requirements.txt 
+}
 
