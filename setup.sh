3e2d1865957c4edddee5d645a96002759615cbc9
piotr-ost <piotr.ostrowski@aol.com>
2021-09-15 13:47:16 +0200

    update setup script

--- setup.sh
+++ setup.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 {
+    sudo apt-get update && 
+    sudo apt-get install git-lfs &&
+    sudo apt-get install cmake &&
+    sudo apt-get install python3-opencv &&
     git clone https://github.com/patrikhuber/eos &&
-    apt-get update && 
-    apt-get install git-lfs &&
-    apt-get install cmake &&
     git-lfs pull && 
     pip install -r requirements.txt 
 
 } || {
 
+    apt-get update && 
+    apt-get install git-lfs &&
+    apt-get install cmake &&
+    apt-get install python3-opencv &&
     git clone https://github.com/patrikhuber/eos &&
-    sudo apt-get update && 
-    sudo apt-get install git-lfs &&
-    sudo apt-get install cmake &&
     git-lfs pull && 
     pip install -r requirements.txt 
 }
